606e80a0cff0046e73bd9a0aca4a886b33d6f681
majedhmoud <majedhmoud28@gmail.com>
2026-08-12 13:01:49 +0300

    syncing edits

--- problem solving/main.js
+++ problem solving/main.js
@@ -1,3 +1,10 @@
-let a = require();
-a = a.ques
-console.log(a);
+// 1. Create a Key-Value Map
+const userRoles = new Map();
+
+// 2. Add some values
+userRoles.set("alice", "admin");
+userRoles.set("bob", "editor");
+
+// 3. Check for keys using .has()
+console.log(userRoles.has("alice")); // Output: true
+console.log(userRoles.has("editor")); // Output: false
